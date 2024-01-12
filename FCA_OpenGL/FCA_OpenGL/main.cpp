7a809ae2015a8ccc07a777e22bad854b430e23bd
Priyanthan <82605375+priyanthan11@users.noreply.github.com>
2024-01-12 23:56:09 +0530

    Creating OpenGL window

--- FCA_OpenGL/FCA_OpenGL/main.cpp
+++ FCA_OpenGL/FCA_OpenGL/main.cpp
@@ -4,5 +4,39 @@
 
 int main()
 {
+	// Initiate the window
+	glfwInit();
+
+	// Letting know which version of opengl we are using
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Creating window object
+	GLFWwindow* window = glfwCreateWindow(800,800,"FCA_OPENGL",NULL,NULL);
+	if (window==NULL)
+	{
+		std::cout << "Failed to create OpenGL window" << std::endl;
+		glfwTerminate();
+		return -1;
+
+	}
+
+	// Use the created window object
+	glfwMakeContextCurrent(window);
+
+	// Window loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// poll the all events of windows
+		glfwPollEvents();
+	}
+
+
+
+	// Destroy the window
+	glfwDestroyWindow(window);
+	// Terminate the window 
+	glfwTerminate();
 	return 0;
 }
